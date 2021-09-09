4a9e21fa4c23017d7595249e8da12a18d47baf9d
Nur Ikhwan Idris Abdul Rahman <nurikhwanidris@gmail.com>
2021-09-09 23:09:08 +0800

    Include the author

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <div class="text-3xl md:text-5xl font-bold mb-3">
       <i class="fa fa-viruses"></i> Covid-19 Tracker
     </div>
-
+    <p>Made by Ikhwan</p>
     <p>
       API by
       <a href="https://covid19api.com" target="_blank" class="text-blue-300"
